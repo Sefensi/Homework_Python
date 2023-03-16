d13fadf8fed111f0cffa533df3489d7f446788c4
Sefensi <sefensi_reload@mail.ru>
2023-03-16 19:20:16 +0500

    Ex_1

---
+++ Homework_1/Ex_1.py
@@ -0,0 +1,11 @@
+# Задача 2: Найдите сумму цифр трехзначного числа.
+
+# *Пример:*
+
+# 123 -> 6 (1 + 2 + 3)
+# 100 -> 1 (1 + 0 + 0) |
+
+numbers = input("Введите число: ")
+print(numbers)
+summary = (int(numbers[0]) + int(numbers[1]) + int(numbers[2]))
+print(summary)
