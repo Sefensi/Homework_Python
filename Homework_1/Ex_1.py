a790d29cab915a1d67a52a15d3501390581daefe
Sefensi <sefensi_reload@mail.ru>
2023-04-25 21:16:50 +0500

    H8_Ex1

--- Homework_1/Ex_1.py
+++ Homework_1/Ex_1.py
@@ -7,5 +7,7 @@
 
 numbers = input("Введите число: ")
 print(numbers)
+second_summary = numbers[0] + numbers[1] + numbers[2]
+print(second_summary)
 summary = (int(numbers[0]) + int(numbers[1]) + int(numbers[2]))
 print(summary)
